36354f63b88666bb9927575a3bf47874bde21281
Alex <cartnix21@gmail.com>
2025-09-11 19:26:07 +0300

    Branch is cleaned

--- frontend/landing/src/assets/svg/CardEfficiencyIcon.tsx
+++
@@ -1,5 +0,0 @@
-export default function CardEfficiencyIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path d="M 5.958 10.563 L 11.375 10.563 M 5.958 6.771 L 11.375 6.771 M 5.958 2.979 L 11.375 2.979 M 1.625 2.979 L 2.167 3.521 L 3.792 1.896 M 1.625 6.771 L 2.167 7.312 L 3.792 5.688 M 1.625 10.563 L 2.167 11.104 L 3.792 9.479" fill="transparent" strokeWidth="0.81" stroke="rgb(255, 255, 255)" strokeLinecap="round" strokeLinejoin="round"></path></svg>
-    )
-}
--- frontend/landing/src/assets/svg/CardShieldIcon.tsx
+++
@@ -1,5 +0,0 @@
-export default function CardShieldIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path d="M 11.326 6.023 C 11.326 8.672 9.403 11.153 6.776 11.879 C 6.597 11.927 6.402 11.927 6.224 11.879 C 3.597 11.153 1.674 8.672 1.674 6.023 L 1.674 3.645 C 1.674 3.201 2.01 2.697 2.427 2.53 L 5.444 1.295 C 6.121 1.018 6.885 1.018 7.562 1.295 L 10.579 2.53 C 10.99 2.697 11.332 3.201 11.332 3.645 Z" fill="transparent" strokeWidth="0.81" stroke="rgb(255, 255, 255)" strokeLinecap="round" strokeLinejoin="round"></path></svg>
-    )
-}
